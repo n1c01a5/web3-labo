8290e06f5ee5af6afe48c718239295a4d5eb3bde
Wagner Nicolas <contact@wagner-nicolas.com>
2021-06-15 19:06:23 +0200

    feat: connect dApp to web3

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [isConnectedWeb3, setIsConnectedWeb3] = useState(false)
+
+  const connectToWeb3 = useCallback(
+    async () => {
+      if(window.ethereum) {
+        try {
+          await window.ethereum.request({method: 'eth_requestAccounts'})
+
+          setIsConnectedWeb3(true)
+        } catch (err) {
+          console.error(err)
+        }
+      } else {
+        alert("Install Metamask")
+      }
+    }
+  )
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,6 +36,12 @@ export default function Home() {
         <h1 className={styles.title}>
           Web3 Labo
         </h1>
+        <h2>Connect Web3</h2>
+        {
+          isConnectedWeb3
+            ? <p>Connected</p>
+            : <button onClick={connectToWeb3}>Connect to web3</button>
+        }
       </main>
     </div>
   )
