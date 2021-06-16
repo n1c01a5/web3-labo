ebf4e7372dba6073821bd40ee0c5de973f9f42c5
Wagner Nicolas <contact@wagner-nicolas.com>
2021-06-16 08:28:23 +0200

    feat: read balance and send eth

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [isConnectedWeb3, setIsConnectedWeb3] = useState(false)
+  const [accounts, setAccounts] = useState([])
+  const [balance, setBalance] = useState(0)
   const [web3, setWeb3] = useState({})
+  const [weiToSend, setWeiToSend] = useState(0)
+  const [addressToSend, setAddressToSend] = useState("")
 
   const connectToWeb3 = useCallback(
     async () => {
@@ -31,8 +35,19 @@ export default function Home() {
 
     setWeb3(web3)
 
-    console.log(web3) // web3 instance
-  }, [isConnectedWeb3])
+    const getAccounts = async () => setAccounts(await web3.eth.getAccounts())
+    const getBalance = async () => setBalance(await web3.eth.getBalance(accounts[0]))
+
+    if (accounts.length == 0) getAccounts()
+    if (accounts.length > 0) getBalance()
+  }, [isConnectedWeb3, accounts])
+
+  const sendEth = useCallback(
+    async () => {
+      await web3.eth.sendTransaction({ from: accounts[0], to: addressToSend, value: weiToSend })
+    },
+    [accounts, addressToSend, weiToSend]
+  )
 
   return (
     <div className={styles.container}>
@@ -52,6 +67,13 @@ export default function Home() {
             ? <p>Connected</p>
             : <button onClick={connectToWeb3}>Connect to web3</button>
         }
+        <h2>Balance</h2>
+        <p>{accounts[0]}</p>
+        <p>{balance}wei</p>
+        <h2>Send ETH</h2>
+        <input type="number" onChange={e => setWeiToSend(e.target.value)} placeholder="Eth in wei" />
+        <input type="text" onChange={e => setAddressToSend(e.target.value)} placeholder="address" />
+        <button onClick={sendEth}>Send Eth</button>
       </main>
     </div>
   )
